f6bfd73ebe742b7e3ea7a1b581ab664d6a42991a
Norman <norman1000@gmail.com>
2020-01-24 17:25:55 +0000

    batch

---
+++ src/batchfunc.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+@author: nk7g14
+
+This script is used for running the XSPEC model 'ulxlc' for a variety of
+different model parameters.
+
+It requires that one has properly set up local models and installed ulxlc
+so that one may use the command "lmod ulxlc" in XSPEC.
+
+This script for some reason does not work in spyder interactive window.
+"""
+import subprocess
+import os
+
+def make_xcm(xcm_filename, parameters, lightcurve_filename):
+    '''
+    Creates xcm script file for use in XSPEC.
+    
+    xcm_filename: name of xcm file with extension
+    parameters: dictionary of model parameters
+    '''
+    F = open(xcm_filename, 'w')
+
+
+    string = f'''lmod ulxlc
+model ulxlc & /*
+newpar 1  {parameters['period']}
+newpar 2  {parameters['phase']}
+newpar 3  {parameters['theta']}
+newpar 4  {parameters['inclination']}
+newpar 5  {parameters['dincl']}
+newpar 6  {parameters['beta']}
+newpar 7  {parameters['dopulse']}
+newpar 8  {parameters['norm']}
+cpd /null
+plot model
+setplot command wdata {lightcurve_filename}
+plot
+exit'''
+
+    F.write(string)
+    F.close()
+
+
+def run_xcm(xcm_filename):
+    '''
+    XCMfile: XCM file including extension
+    '''
+    devnull = open(os.devnull, 'w')
+    subprocess.call([f'xspec - {xcm_filename}'], shell=True, stdout=devnull)
+
+def append_parameters_to_lightcurve_files(lightcurve_filename, system_id ,parameters):
+    with open(lightcurve_filename, "a") as myfile:
+        myfile.write(f"system_id:{system_id}\n")
+        for k, v in parameters.items():
+            myfile.write(f'{k}: {v}\n')
+
+def run_ulxlc(xcm_filename, parameters, system_id, lightcurve_filename):
+    make_xcm(xcm_filename, parameters, lightcurve_filename)
+    run_xcm(xcm_filename)
+    append_parameters_to_lightcurve_files(lightcurve_filename, system_id ,parameters)
+    os.remove(xcm_filename)
---
+++ src/simulation_runner.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 24 16:57:46 2020
+
+@author: nk7g14
+"""
+
+import numpy as np
+from uuid import uuid4
+
+import batchfunc
+from auxil import load_systems_dataframe
+
+systems_df = load_systems_dataframe(True, True, True)
+
+systems_df['theta_half_deg']
+
+inclinations = np.arange(0,91)
+dincls = np.arange(0,46)
+
+ulxlc_parameters = {'period': 10.0,
+                'phase': 0.0,
+                'theta': 10,
+                'inclination': 0,
+                'dincl': 12,
+                'beta': 0.2,
+                'dopulse': 0,
+                'norm': 1.0}
+
+
+
+
+while True:
+    
+    sample = systems_df['theta_half_deg'].sample()
+    selected_theta = sample.values[0]
+    selected_id = sample.index[0]
+    ulxlc_parameters['theta'] = selected_theta
+    for i in inclinations:
+        ulxlc_parameters['inclination'] = i
+        for d in dincls:
+            ulxlc_parameters['dincl'] = d
+
+            xcm_n = str(uuid4())+'.xcm'
+            lc_n = 'new_curves/'+str(uuid4())+'.txt'
+            batchfunc.run_ulxlc(xcm_n, ulxlc_parameters, selected_id, lc_n)
